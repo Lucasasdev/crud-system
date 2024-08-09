06ba17cb145318c0c5956a0ba5362931d9a35c05
Lucas Alves <lucasasdev@gmail.com>
2024-08-09 10:16:50 -0300

    feat: add tailwind and shadcn config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <p>Hello world!</p>;
+  return (
+    <>
+      <p className="m-2 text-red-500">Hello world!</p>
+      <Button className="m-2">start</Button>
+    </>
+  );
 }
 
 export default App;
