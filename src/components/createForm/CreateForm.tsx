ac4e8ff7072804638d6f46f5576ccf9f6ce56467
Lucas Alves <lucasasdev@gmail.com>
2024-08-14 21:44:08 -0300

    feat: add hidden effect to create form

--- src/components/createForm/CreateForm.tsx
+++ src/components/createForm/CreateForm.tsx
@@ -8,6 +8,9 @@ const CreateForm = () => {
   const [description, setDescription] = useState("");
   const [price, setPrice] = useState(0);
   const [stock, setStock] = useState(0);
+
+  const [isVisible, setIsVisible] = useState(false);
+
   const handleCreateClick: React.FormEventHandler<HTMLFormElement> = async (
     event
   ) => {
@@ -28,55 +31,74 @@ const CreateForm = () => {
     setStock(0);
   };
 
+  const handleAddProductClick = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div className="w-full min-w-[250px] flex justify-center">
-      <form
-        onSubmit={handleCreateClick}
-        className="bg-secondary flex flex-col m-5 p-4 gap-2 rounded-lg max-w-[800px] w-full"
-      >
-        <div>
-          <label htmlFor="fname">Name</label>
-          <Input
-            id="fname"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          ></Input>
-        </div>
-        <div>
-          <label htmlFor="fdescription">Description</label>
-          <Input
-            id="fdescription"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></Input>
-        </div>
-        <div>
-          <label htmlFor="fprice">Price</label>
-          <Input
-            type="number"
-            value={price}
-            min={0}
-            step="0.01"
-            id="fprice"
-            onChange={(e) => setPrice(Number(e.target.value))}
-          ></Input>
-        </div>
-        <div>
-          <label htmlFor="fstock">Stock</label>
-          <Input
-            type="number"
-            value={stock}
-            min={0}
-            id="fstock"
-            onChange={(e) => setStock(Number(e.target.value))}
-          ></Input>
-        </div>
-        <div>
-          <Button type="submit" className="font-bold">
-            Create
-          </Button>
-        </div>
-      </form>
+    <div className="w-full min-w-[250px] flex flex-col items-center">
+      {isVisible && (
+        <form
+          onSubmit={handleCreateClick}
+          className="bg-secondary flex flex-col m-5 p-5 gap-2 rounded-lg max-w-[800px] w-full"
+        >
+          <div>
+            <label htmlFor="fname">Name</label>
+            <Input
+              id="fname"
+              value={name}
+              placeholder="Nome no produto"
+              onChange={(e) => setName(e.target.value)}
+            ></Input>
+          </div>
+          <div>
+            <label htmlFor="fdescription">Description</label>
+            <Input
+              id="fdescription"
+              value={description}
+              placeholder="Descrição breve do produto"
+              onChange={(e) => setDescription(e.target.value)}
+            ></Input>
+          </div>
+          <div>
+            <label htmlFor="fprice">Price</label>
+            <Input
+              type="number"
+              value={price}
+              min={0}
+              step="0.01"
+              placeholder="Preço total do produto"
+              id="fprice"
+              onChange={(e) => setPrice(Number(e.target.value))}
+            ></Input>
+          </div>
+          <div>
+            <label htmlFor="fstock">Stock</label>
+            <Input
+              type="number"
+              value={stock}
+              min={0}
+              placeholder="Quantidade em estoque"
+              id="fstock"
+              onChange={(e) => setStock(Number(e.target.value))}
+            ></Input>
+          </div>
+          <div>
+            <Button type="submit" className="font-bold">
+              Create
+            </Button>
+          </div>
+        </form>
+      )}
+      <div className="max-w-[800px] w-full">
+        <Button
+          type="button"
+          className="font-bold m-5"
+          onClick={handleAddProductClick}
+        >
+          Add product
+        </Button>
+      </div>
     </div>
   );
 };
